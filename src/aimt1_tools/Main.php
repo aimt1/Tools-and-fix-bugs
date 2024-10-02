f16396d840d8216ed246f9cdd99568fc5c23c837
aimt1 <almatramazanov37@gmail.com>
2024-10-02 13:19:34 +0500

    Update Main.php

--- src/aimt1_tools/Main.php
+++ src/aimt1_tools/Main.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace aimt1_tools;
 
@@ -10,7 +10,7 @@ use function array_diff;
 use function scandir;
 
 class Main extends PluginBase {
-    private $config;
+    private Config $config;
 
     public function onEnable() {
         @mkdir($this->getDataFolder());
@@ -18,17 +18,17 @@ class Main extends PluginBase {
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $this->getLogger()->info("Tools and fix bugs enabled!");
 
-        foreach(array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]) as $levelName){
-			if($this->getServer()->loadLevel($levelName)){
-				$this->getLogger()->debug("Successfully loaded §6${levelName}");
-			}
+        foreach(array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]) as $levelName) {
+            if($this->getServer()->loadLevel($levelName)) {
+                $this->getLogger()->debug("Successfully loaded §6{$levelName}");
+            }
         }
     }
 
-    public function getConfigValue($key) {
+    public function getConfigValue(string $key) {
         return $this->config->get($key);
     }
-    
+
     public function onDisable() {
         $this->getLogger()->info("Tools and fix bugs disabled!");
     }
