c5cfbb2a6cafd81d0552e48bc0919d22d45fb548
aimt1 <almatramazanov37@gmail.com>
2024-10-02 12:55:57 +0500

    Create Main.php

---
+++ src/aimt1_tools/Main.php
@@ -0,0 +1,24 @@
+<?php
+namespace YourNamespace;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class YourPluginClass extends PluginBase {
+    private $config;
+
+    public function onEnable() {
+        @mkdir($this->getDataFolder());
+        $this->saveResource("config.yml");
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        $this->getLogger()->info("Tools and fix bugs enabled!");
+    }
+
+    public function getConfigValue($key) {
+        return $this->config->get($key);
+    }
+    
+    public function onDisable() {
+        $this->getLogger()->info("Tools and fix bugs disabled!");
+    }
+}
