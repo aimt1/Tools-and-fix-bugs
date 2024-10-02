2b314bea9dbe9c99a29c60f357406e7c4f05c549
aimt1 <almatramazanov37@gmail.com>
2024-10-02 13:12:26 +0500

    Update Main.php

--- src/aimt1_tools/Main.php
+++ src/aimt1_tools/Main.php
@@ -1,8 +1,13 @@
 <?php
+declare(strict_types = 1);
+
 namespace aimt1_tools;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+
+use function array_diff;
+use function scandir;
 
 class Main extends PluginBase {
     private $config;
@@ -12,6 +17,12 @@ class Main extends PluginBase {
         $this->saveResource("config.yml");
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $this->getLogger()->info("Tools and fix bugs enabled!");
+
+        foreach(array_diff(scandir($this->getServer()->getDataPath() . "worlds"), ["..", "."]) as $levelName){
+			if($this->getServer()->loadLevel($levelName)){
+				$this->getLogger()->debug("Successfully loaded §6${levelName}");
+			}
+        }
     }
 
     public function getConfigValue($key) {
